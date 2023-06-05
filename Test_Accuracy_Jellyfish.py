442d9b917dedf469627b9503d56b97fd30131160
siyonasuresh <114272611+siyonasuresh@users.noreply.github.com>
2023-06-05 15:56:57 -0700

    Create Test_Accuracy_Jellyfish.py

---
+++ Test_Accuracy_Jellyfish.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+  
+kmers = []
+counts = []
+  
+f = open('realoutput.txt','r')
+for row in f:
+    if '>' in line: 
+        counts.append(int(row[1:]))
+else: 
+    kmers.append(row)
+  
+plt.bar(kmers, counts, color = 'g', label = 'File Data')
+  
+plt.xlabel('Kmers', fontsize = 12)
+plt.ylabel('Counts', fontsize = 12)
+  
+plt.title('Kmer Counts for Jellyfish', fontsize = 20)
+plt.legend()
+plt.show()
