927af308395fe6c5cc1928b21373fe114755a80d
sriya p <122697432+sriya224@users.noreply.github.com>
2023-06-06 16:02:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+# setup tool to create blobfish package for users to download
+from setuptools import setup, find_packages
 
+setup(
+    name='your-package-name',
+    version='1.0',
+    description='The blobfish package creates a txt file with a table of kmer counts from a given fasta sequence.',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'blobfish = blobfish.py:main',
+        ],
+    },
+)
