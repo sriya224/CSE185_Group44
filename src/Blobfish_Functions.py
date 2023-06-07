432d8d027e92512f817a853901a223e70c6d9dfd
sriya p <122697432+sriya224@users.noreply.github.com>
2023-06-06 20:10:31 -0700

    Update Blobfish_Functions.py

--- src/Blobfish_Functions.py
+++ src/Blobfish_Functions.py
@@ -9,22 +9,22 @@
 from Bio import SeqIO
 
 ## This function reads a sequence from the inputted fasta file and outputs a file containing the desired kmer count table
-# Parameters: 
-# input_fa: the user given fasta file
-# kmer_length: int with the number of bases that are in each kmer 
-# output: the user given output text file 
-# directionality: boolean value to either consider directionality or not 
-# filter_kmer: int value with the threshold value for filtering, if filtering is specified by the user 
+    ## PARAMETERS: 
+    # input_fa: the user given fasta file
+    # kmer_length: int with the number of bases that are in each kmer 
+    # output: the user given output text file 
+    # directionality: boolean value to either consider directionality or not 
+    # filter_kmer: int value with the threshold value for filtering, if filtering is specified by the user 
 def generateTable(input_fa, kmer_length, output, directionality, filter_kmer, filter_rec):
 
-    # seq_file has input fasta file's data
+    # Seq_file has input fasta file's data
     seq_file = input_fa
 
     #implement hashtable using dictionary
     hashtable = {}
 
-    # extract the sequence from the fasta file
-    # utilizes the SeqIO package 
+    # Extract the sequence from the fasta file
+    # Utilizes the SeqIO package 
     for sequ in SeqIO.parse(open(seq_file),'fasta'):
         sequence = str(sequ.seq)
     
@@ -37,7 +37,6 @@ def generateTable(input_fa, kmer_length, output, directionality, filter_kmer, fi
         count_kmers(sequence, hashtable, kmer_length)
         
     ## FILTER
-    
     # If both filter options are chosen, override filter by count and only filter by recommended
     # Else if filter by recommended option is chosen run filter_auto() else run filter_count()
     if(filter_kmer > 0 & filter_rec):
@@ -72,9 +71,9 @@ def count_kmers(sequence, hashtable, kmer_length):
         
 
 ## Function to count kmers while ignoring directionality / considering the reverse strand when counting
-# Parameter 
-# sequence parameter takes the user's sequence from which we should count kmers 
-# kmer parameter takes user's desired kmer size
+    # Parameters
+    # sequence parameter takes the user's sequence from which we should count kmers 
+    # kmer parameter takes user's desired kmer size
 def ignore_directionality(sequence, kmer_length, hashtable):
     nucleotides = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
     
@@ -88,13 +87,10 @@ def ignore_directionality(sequence, kmer_length, hashtable):
         else: 
             hashtable[kmer_complement] = hashtable.get(kmer_complement, 0) + 1
 
-#function to filter out low frequency kmers
-# output file should be from previous commands in our program and contains calculated kmers and their frequencies
-# threshold parameter is an integer value given by the user. Any kmer frequency lower than the threshold will be dropped
-# output file should be from previous commands in our program and contains calculated kmers and their frequencies
-# threshold parameter is an integer value given by the user. Any kmer frequency lower than the threshold will be dropped
-# output file should be from previous commands in our program and contains calculated kmers and their frequencies
-# threshold parameter is an integer value given by the user. Any kmer frequency lower than the threshold will be dropped
+# Function to filter out low frequency kmers by inputted count
+   ## Parameters:
+    # hashtable = dictionary
+    # threshold = inputted filtering count
 def filter_count(hashtable, threshold):
     inside = False
     mylist = []
@@ -110,6 +106,9 @@ def filter_count(hashtable, threshold):
     hashtable = outputhash
     return hashtable
 
+# Function to filter out low frequency kmers automatically
+   ## Parameters:
+    # hashtable = dictionary
 def filter_auto(hashtable):
     outputhash = {}
     for key, value in hashtable.items(): 
@@ -117,41 +116,30 @@ def filter_auto(hashtable):
             outputhash[value] += 1
         else:
             outputhash[value] = 1
-    
-    print(outputhash)
         
     valleycount = find_valley(outputhash)
     
-    print(valleycount)
+    print("Your recommended filtering count is" + str(valleycount))
     
     if(valleycount == None):
         print("All kmer counts are of low frequency!")
         return
     
-    
     keys_to_delete = []  # List to store keys to be deleted
-    
-    print("here")
-    print(outputhash)
 
     for key, value in outputhash.items():
         if key <= valleycount:  
             keys_to_delete.append(key)  
 
-    print(keys_to_delete)
-
     for key in keys_to_delete:
         del outputhash[key]
-    
     
     hashtable = outputhash
     return hashtable
         
-
+# Helper method
 def find_valley(hashtable):
     valley = list(hashtable.values())[0]
-    print('hashtable')
-    print(hashtable)
     for key in hashtable:
         if(hashtable[key] - valley > 0):
             return key
